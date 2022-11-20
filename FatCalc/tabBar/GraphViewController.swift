ac5f16d535a878683ee82933440cb0f4e7c4f6e9
swego26 <peretzsegev@gmail.com>
2022-11-20 21:41:23 +0200

    add two charts

--- FatCalc/tabBar/GraphViewController.swift
+++ FatCalc/tabBar/GraphViewController.swift
@@ -10,7 +10,8 @@ import Charts
 
 class GraphViewController: UIViewController, ChartViewDelegate {
 
-      
+    var sets : [LineChartDataSet] = [] //global var for lines
+
     lazy var lineChartView: LineChartView = {
         let chartView = LineChartView()
         chartView.leftAxis.axisLineColor = .black
@@ -33,7 +34,8 @@ class GraphViewController: UIViewController, ChartViewDelegate {
         //title = currentControllerName
         print("current controller is \(currentControllerName)")
         
-        loadFats() //(every time user check chart)
+        loadWeight()
+        loadFats()
     }
 
     private func chartSetup () {
@@ -58,9 +60,38 @@ class GraphViewController: UIViewController, ChartViewDelegate {
         print(entry.y)
     }
     
+    func loadWeight () {
+        var chartEntries : [ChartDataEntry] = []
+        var entryIndex : Double = 0.0
+        if let loadedData = Funcs.shared.loadFromCoreData() {
+            for i in loadedData {
+                entryIndex += 1
+                let weight = Double(i.weight)
+                let weightChartEntry = ChartDataEntry(x: entryIndex, y: weight)
+                chartEntries.append(weightChartEntry)
+            }
+            #warning("specify set")
+            setData(chartEntries, name: "weight", color: .white)
+        } else {print("No weight data")}
+    }
+    
+    func loadFats () {
+        var chartEntries : [ChartDataEntry] = []
+        var entryIndex : Double = 0.0
+        if let loadedData = Funcs.shared.loadFromCoreData() {
+            for i in loadedData {
+                entryIndex += 1
+                let fat = Double(i.fatPercentage)
+                let fatsChartEntry = ChartDataEntry(x: entryIndex, y: fat)
+                chartEntries.append(fatsChartEntry)
+            }
+            #warning("specify set")
+            setData(chartEntries, name: "Fat %", color: .gray)
+
+        } else {print("No fats data")}
+    }
    
-    private func loadFats () {
-        #warning("load all entries as [Entry] with weight and fat percent properties")
+    /*private func loadAll () {
         //1. "everytime theres value in [Entry], it also load all weights"
         //2. "show dates insted of numbers at bottom axis"
         //3. "attach weight to fats if on the same date"
@@ -99,28 +130,31 @@ class GraphViewController: UIViewController, ChartViewDelegate {
         } else {
             print("Chart has no entries to load!")
         }
-    }
+    }*/
     
    
     //2. set data to chart
-    private func setData (_ chartDataEntryArray: [ChartDataEntry]) {
+    private func setData (_ chartDataEntryArray: [ChartDataEntry], name: String, color:UIColor) {
+        //add chart data to a new set everytime func is called
+        
         //take the entries and insert into a data set and name it.
-        let fatsSet = LineChartDataSet(entries: chartDataEntryArray, label: "Fat %")
+        let newSet = LineChartDataSet(entries: chartDataEntryArray, label: name)
         
         //set the set's attributes
-        fatsSet.mode = .cubicBezier //little bit rounded
-        fatsSet.circleHoleRadius = 0.05 //circle radius
-        fatsSet.drawFilledEnabled = true //add fill
-        fatsSet.fillColor = .white //fill color
-        fatsSet.fillAlpha = 0.4 //fill alpha
+        newSet.mode = .horizontalBezier //line style
+        newSet.circleHoleRadius = 0.03 //circle radius
+        newSet.drawFilledEnabled = true //add fill
+        newSet.fillColor = color //fill color
+        newSet.fillAlpha = 0.4 //fill alpha
+        
+        sets.append(newSet)
         
         //make LineChartData from the data set. (LineData specificly)
-        let fatsData = LineChartData(dataSet: fatsSet)
+        let newData = LineChartData(dataSets: sets)
         
         //attach data to chart
-        lineChartView.data = fatsData
-        
-        
+        lineChartView.data = newData
+    
      
     }
     
